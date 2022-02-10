5662abcef8fbaf1249fb10ff9b8356ebf41e5a0c
Yothin Muangsommuk <m@yothinix.com>
2022-02-10 13:50:51 +0700

    Adding run method

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Lox {
     public static void main(String[] args) throws IOException {
@@ -33,6 +34,15 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
         }
     }
 }
