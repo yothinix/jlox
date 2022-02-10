1d6260aba1e251591eb47cc738a1ca7cf48c0728
Yothin Muangsommuk <m@yothinix.com>
2022-02-10 13:36:30 +0700

    Initial jLox project

---
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,24 @@
+package com.craftinginterpreters.lox;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Lox {
+    public static void main(String[] args) {
+        if (args.length > 1) {
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length == 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    public static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+}
